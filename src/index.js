049d2ee0e35d73c0b11a80c0da7e0ab9ce54ff71
maxjacobzander <maxjacobzander@gmail.com>
2020-11-30 15:59:05 -0600

    commit tree 65726039c3e402f1e828b3042be2ca494ad28a29

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log('%c HI', 'color: firebrick')
+
+function getImages(){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(data => addImageToPage(data))
+}
+
+function addImageToPage(){
+    image.forEach(function(img))
+}
